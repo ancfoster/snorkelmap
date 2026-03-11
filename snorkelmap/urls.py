f956e80e734618d0bd679c1843defa76e7c33d9d
ancfoster <65979715+ancfoster@users.noreply.github.com>
2026-03-11 13:19:26 +0000

    Add logout link
    - Configure one-step Django allauth logout
    - Add logout link to nav bar

--- snorkelmap/urls.py
+++ snorkelmap/urls.py
@@ -5,7 +5,10 @@ from allauth.account.views import LoginView, SignupView
 from .views import home
 
 urlpatterns = [
+    path('smadmin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name="sm_login"),
+    path('create-snorkelmap-account/', SignupView.as_view(), name="custom_signup"),
+    path('account/', include('allauth.urls')),
     path("", home, name="home"),
-    path('smadmin/', admin.site.urls)
-    #path('account/', include('allauth.urls'))
+    path("", include('snorkelusers.urls'))
 ]
