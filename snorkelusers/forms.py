c5f18c43b09d10910e2c0b29dc586391585a503d
ancfoster <65979715+ancfoster@users.noreply.github.com>
2026-05-04 20:24:43 +0100

    Modify snorkelusers forms.py
    Customise the  CustomSignupForm class to:
    - add accepted terms and conditions check box
    - have correct order of elements
    - have correct field labels
    - remove django allauth forced help text description for password criteria

--- snorkelusers/forms.py
+++ snorkelusers/forms.py
@@ -3,12 +3,30 @@ from allauth.account.forms import SignupForm, LoginForm
 from turnstile.fields import TurnstileField
 
 class CustomSignupForm(SignupForm):
-    first_name = forms.CharField(label="What should we call you?",required=True,widget=forms.TextInput(attrs={'placeholder': 'Enter your first name'}))
-    #turnstile = TurnstileField()
+    first_name = forms.CharField(
+        label="What should we call you?",
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your first name'})
+    )
+    accepted_terms_conditions = forms.BooleanField(
+        required=True,
+        label='I accept the terms and conditions.',
+        widget=forms.CheckboxInput()
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = None
+        self.fields['password1'].label = 'Password'
+        self.fields['password2'].label = 'Confirm Password'
+        self.fields['email'].label = 'Email'
+        self.fields['username'].label = 'Create a username (publicly displayed)'
+        self.fields.move_to_end('accepted_terms_conditions')
 
     def save(self, request):
         user = super().save(request)
         user.first_name = self.cleaned_data['first_name']
+        user.accepted_terms_conditions = self.cleaned_data['accepted_terms_conditions']
         user.save()
         return user
     
