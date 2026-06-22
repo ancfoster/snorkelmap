1aae7f865a2926292d4c1bedb2a112bf4cb1c3c7
ancfoster <65979715+ancfoster@users.noreply.github.com>
2026-06-22 19:38:28 +0100

    Update allowed host values

--- snorkelmap/local.py
+++ snorkelmap/local.py
@@ -3,13 +3,15 @@ from .settings import *
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['snorkelmap.local', 'localhost', '127.0.0.1']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 TURNSTILE_SECRET = '1x0000000000000000000000000000000AA'  # Cloudflare's test key
 TURNSTILE_SITEKEY = '1x00000000000000000000AA'  # Cloudflare's test key
+
+INSTALLED_APPS.append('django_extensions')
 
 STORAGES = {
     "default": {
