6ef3c9dd40cef36cd372d81a8a19eeecf666e71b
ancfoster <65979715+ancfoster@users.noreply.github.com>
2026-09-12 09:54:45 +0100

    Initiate admin_moderation & review apps

---
+++ snorkel_admin_moderation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SnorkelAdminModerationConfig(AppConfig):
+    name = 'snorkel_admin_moderation'
---
+++ snorkel_review/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SnorkelReviewConfig(AppConfig):
+    name = 'snorkel_review'
